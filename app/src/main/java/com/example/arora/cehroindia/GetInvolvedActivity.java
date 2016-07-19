e16d720d86a874a7e8e0e173ff04a592e8310812
rajat1994 <rajatarora25@yahoo.in>
2016-07-19 12:12:41 +0530

    added tabs for get involved activity

--- app/src/main/java/com/example/arora/cehroindia/GetInvolvedActivity.java
+++ app/src/main/java/com/example/arora/cehroindia/GetInvolvedActivity.java
@@ -1,5 +1,7 @@
 package com.example.arora.cehroindia;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class GetInvolvedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_us);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+
+        GetInvolvedAdapter adapter = new GetInvolvedAdapter(this,getSupportFragmentManager());
+
+
+        viewPager.setAdapter(adapter);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
