979b73840ba9e5bccee7c4e3cf155b289e9982d1
Mac <mac@Macs-MacBook-Pro-2.local>
2016-07-23 20:12:42 +0530

    Completed Our Program with Card View

--- app/src/main/java/com/example/arora/cehroindia/RecyclerAdapter.java
+++ app/src/main/java/com/example/arora/cehroindia/RecyclerAdapter.java
@@ -2,6 +2,8 @@ package com.example.arora.cehroindia;
 
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.media.tv.TvContract;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.arora.cehroindia.EducatingIndiaProg;
+import com.example.arora.cehroindia.PadhoPadhaoProgramActivity;
 
 import org.w3c.dom.Text;
 
@@ -24,19 +28,23 @@ import java.util.List;
  * Created by mac on 20/07/16.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+public class
+RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
     private static final String TAG = "ContactsRecyclerAdapter";
 
     private ArrayList<Program> mPrograms = new ArrayList<Program>();
-    private String[] eventName;
-    private String[] eventBrief;
-    private OnItemClickListener listener;
+    private Context mContext;
+    public static CardView cardView;
+    public static int position;
+    public String currentTitle;
 
-    public RecyclerAdapter(ArrayList<Program> programs) {
+
+    public RecyclerAdapter(Context context, ArrayList<Program> programs) {
+        this.mContext = context;
         mPrograms = programs;
     }
 
-    public ViewHolder onCreateViewHolder(ViewGroup parent,int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
@@ -47,20 +55,21 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-       Program program = mPrograms.get(position);
+        Program program = mPrograms.get(position);
 
         TextView title = holder.titleTextView;
         ImageView programImage = holder.imageView;
         TextView description = holder.descriptionTextView;
         holder.currentProgram = program;
 
+        currentTitle = program.getTitle();
         title.setText(program.getTitle());
         description.setText(program.getDescription());
         Glide.with(programImage.getContext())
                 .load(program.getimageResourceId())
                 .fitCenter()
                 .into(programImage);
-        
+
 
     }
 
@@ -69,47 +78,71 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         return mPrograms.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView titleTextView;
         public TextView descriptionTextView;
         public ImageView imageView;
         public Program currentProgram;
-        public CardView cardView;
-        private OnItemClickListener listener;
+
 
         public ViewHolder(final View itemView) {
             super(itemView);
 
+            cardView = (CardView) itemView.findViewById(R.id.programItem);
             titleTextView = (TextView) itemView.findViewById(R.id.program_title);
             imageView = (ImageView) itemView.findViewById(R.id.program_image);
             descriptionTextView = (TextView) itemView.findViewById(R.id.program_description);
-            cardView  = (CardView) itemView.findViewById(R.id.programItem) ;
 
-            cardView.setOnClickListener(new View.OnClickListener(){
+
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(itemView.getContext(),currentProgram.getTitle(),Toast.LENGTH_LONG).show();
-                    if(listener!=null)
-                        listener.onItemClick(titleTextView.getText().toString(),descriptionTextView.getText().toString());
+                   final  Intent intent;
 
+
+
+                    switch (currentProgram.getTitle()) {
+                        case "Educating India":
+                            intent = new Intent(v.getContext(), EducatingIndiaProg.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+                        case "Padho-Padhao Programme":
+                           intent = new Intent(v.getContext(), PadhoPadhaoProgramActivity.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+                        case "Livelihood Programme":
+                            intent = new Intent(v.getContext(), LivelihoodProgActivity.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+                        case "Health And Hygeine Programme":
+                            intent = new Intent(v.getContext(), TargetTwentyProgActivity.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+                        case "Target Twenty 20":
+                            intent = new Intent(v.getContext(), HealthProgActivity.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+                        case "Artistic India":
+                            intent = new Intent(v.getContext(), ArtisticIndiaActivity.class);
+                            v.getContext().startActivity(intent);
+                            break;
+
+
+
+                    }
                 }
             });
+
         }
 
 
-}    public RecyclerAdapter(String[] eventName, String[] eventBrief) {
-        this.eventName = eventName;
-        this.eventBrief = eventBrief;
-
     }
 
 
-    public void setOnItemClickListener(OnItemClickListener listener){
-        this.listener = listener;
-    }
-
-    public interface OnItemClickListener{
-        public void onItemClick(String textName, String textViewBrief);
-    }
 }
